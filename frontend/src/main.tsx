648789734498131b3ed043bf0a37f98e2ea5b09b
Marcos Avila <marcos.seocuba@gmail.com>
2024-10-02 21:31:23 -0400

    💣 Remove ErrorPage component and update main.tsx to reflect changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import HeaderDashboard from './routes/main_doctor.tsx'
 import DoctorDashboard from './routes/dashboard_main.tsx'
-import ErrorPage from './components/error-page.tsx';
+import ErrorPage from './error-page.tsx';
 import PatientDetail from './routes/patient_detail.tsx';
 import PatientList from './routes/all_patients.tsx'
 import './index.css'
@@ -17,15 +17,20 @@ const router = createBrowserRouter([
     element: <HeaderDashboard />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <DoctorDashboard />},
       {
-        path: "patients/:patientId",
-        element: <PatientDetail />,
-        errorElement: <ErrorPage />
-      },
-      {
-        path: "patients",
-        element: <PatientList />
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <DoctorDashboard />},
+          {
+            path: "patients/:patientId",
+            element: <PatientDetail />,
+            errorElement: <ErrorPage />
+          },
+          {
+            path: "patients",
+            element: <PatientList />
+          }
+        ]
       }
     ]
   },
